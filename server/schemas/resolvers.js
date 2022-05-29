9aacd34b10212dfc66273312239f6f4d6ceffa95
pdhende <pdhendework@gmail.com>
2022-05-29 00:17:55 -0400

    Updated typeDefs.js and resolvers. js to define the schema and Query

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,5 +1,21 @@
 const { Book, User } = require('../models');
 
-const resolvers = {};
+const resolvers = {
+    Query: {
+        me: async (parent, { _id, username }) => {
+            const params = {};
+
+            if (_id) {
+                params._id = _id;
+            }
+
+            if (username) {
+                params.username = username;
+            }
+            return await User.findOne(params).populate('savedBooks');
+        }
+
+    },
+};
 
 module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,5 +1,30 @@
-// const { gql } = require('apollo-server-express');
+const { gql } = require('apollo-server-express');
 
-// const typeDefs = gql``;
+const typeDefs = gql`
+type User {
+    _id: ID!
+    username: String!
+    email: String!
+    bookCount: [Int]
+    savedBooks: [Book]!
+}
 
-// module.exports = typeDefs;
+type Book {
+    authors: [String]
+    description: String!
+    bookId: String!
+    image: String
+    link: String
+    title: String!
+}
+
+type Auth {
+    token: ID!
+    user: User
+}
+
+type Query {
+    me(_id: ID, username: String): User
+}`
+
+module.exports = typeDefs;
